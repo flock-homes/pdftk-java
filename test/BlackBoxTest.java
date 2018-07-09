9deeef31a103e1cbfd4abdb4045888032e7aad7c
Marc Vinyals <marc.vinyals@tifr.res.in>
2018-07-09 13:23:28 +0530

    Proper test

--- test/BlackBoxTest.java
+++ test/BlackBoxTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.assertEquals;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.nio.file.Files;
@@ -7,13 +11,17 @@ import java.nio.file.Paths;
 import java.io.IOException;
 
 public class BlackBoxTest {
+  @Rule
+  public final SystemOutRule systemOutRule = new SystemOutRule().muteForSuccessfulTests();
+
+  @Rule
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+  
   @Test
   public void dump_data() throws IOException {
-    ByteArrayOutputStream stdout = new ByteArrayOutputStream();
-    System.setOut(new PrintStream(stdout));
-    pdftk.main(new String[]{"files/blank.pdf", "dump_data_utf8"});
-    String data = stdout.toString();
-    String expectedData = new String(Files.readAllBytes(Paths.get("files/blank.data")));
-    assertEquals(expectedData, data);
+    exit.expectSystemExitWithStatus(0);
+    pdftk.main(new String[]{"test/files/blank.pdf", "dump_data_utf8"});
+    String expectedData = new String(Files.readAllBytes(Paths.get("test/files/blank.data")));
+    assertEquals(expectedData, systemOutRule.getLog());    
   }
 };
