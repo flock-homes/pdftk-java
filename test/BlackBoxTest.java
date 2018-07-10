42406bc8c26a8c061b185644405ddce41782c03e
Marc Vinyals <marc.vinyals@tifr.res.in>
2018-07-10 18:22:09 +0530

    Cleanup

--- test/BlackBoxTest.java
+++ test/BlackBoxTest.java
@@ -4,24 +4,27 @@ import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
 
 public class BlackBoxTest {
   @Rule
-  public final SystemOutRule systemOutRule = new SystemOutRule().muteForSuccessfulTests();
+  public final SystemOutRule systemOutRule =
+    new SystemOutRule().muteForSuccessfulTests();
 
   @Rule
   public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+  public String slurp(String filename) throws IOException {
+    return new String(Files.readAllBytes(Paths.get(filename)));
+  }
   
   @Test
   public void dump_data() throws IOException {
     exit.expectSystemExitWithStatus(0);
     pdftk.main(new String[]{"test/files/blank.pdf", "dump_data_utf8"});
-    String expectedData = new String(Files.readAllBytes(Paths.get("test/files/blank.data")));
+    String expectedData = slurp("test/files/blank.data");
     assertEquals(expectedData, systemOutRule.getLog());    
   }
 };
