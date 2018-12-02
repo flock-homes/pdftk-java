5eff5eb2a4b24ffced39d939ec148eabe063fb7e
Marc Vinyals <vinyals@kth.se>
2018-12-02 17:43:55 +0530

    Add tests for rotation keywords

--- test/BlackBoxTest.java
+++ test/BlackBoxTest.java
@@ -42,5 +42,41 @@ public class BlackBoxTest {
     byte[] expectedData = slurpBytes("test/files/cat-refs-refsalt.pdf");
     assertEquals(expectedData, systemOutRule.getLogAsBytes());
   }
-  
+
+  @Test
+  public void cat_rotate_page_no_op() throws IOException {
+    exit.expectSystemExitWithStatus(0);
+    pdftk.main(new String[]{"test/files/blank.pdf",
+                            "cat", "1north", "output", "-"});
+    byte[] expectedData = slurpBytes("test/files/blank.pdf");
+    assertEquals(expectedData, systemOutRule.getLogAsBytes());
+  }
+
+  @Test
+  public void cat_rotate_range_no_op() throws IOException {
+    exit.expectSystemExitWithStatus(0);
+    pdftk.main(new String[]{"test/files/blank.pdf",
+                            "cat", "1-1north", "output", "-"});
+    byte[] expectedData = slurpBytes("test/files/blank.pdf");
+    assertEquals(expectedData, systemOutRule.getLogAsBytes());
+  }
+
+  @Test
+  public void cat_rotate_page() throws IOException {
+    exit.expectSystemExitWithStatus(0);
+    pdftk.main(new String[]{"test/files/blank.pdf",
+                            "cat", "1east", "output", "-"});
+    byte[] expectedData = slurpBytes("test/files/blank.pdf");
+    assertEquals(expectedData, systemOutRule.getLogAsBytes());
+  }
+
+  @Test
+  public void cat_rotate_range() throws IOException {
+    exit.expectSystemExitWithStatus(0);
+    pdftk.main(new String[]{"test/files/blank.pdf",
+                            "cat", "1-1east", "output", "-"});
+    byte[] expectedData = slurpBytes("test/files/blank.pdf");
+    assertEquals(expectedData, systemOutRule.getLogAsBytes());
+  }
+
 };
