87b06931a380c726b6f4a197069531cc5e45e387
Marc Vinyals <marc.vinyals@tifr.res.in>
2019-05-20 18:24:00 +0530

    Update version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [3.0.5] - 2019-05-20
+
+### Fixed
+ - Do not print error if PdfID is missing.
+ - Crash in burst with certain inputs (java.lang.NullPointerException).
+
 ## [3.0.4] - 2019-05-02
 
 ### Fixed
--- java/com/gitlab/pdftk_java/pdftk.java
+++ java/com/gitlab/pdftk_java/pdftk.java
@@ -35,7 +35,7 @@ import java.util.Scanner;
 public class pdftk {
 
   /* TODO: should read from compiler */
-  static final String PDFTK_VER = "3.0.4-git";
+  static final String PDFTK_VER = "3.0.5";
   static final boolean ASK_ABOUT_WARNINGS = false;
 
   static String prompt_for_password(String pass_name, String pass_app) {
