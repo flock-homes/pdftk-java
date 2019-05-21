cb656c3ee62c35ba2ae40d19ab73dd0e426011da
Marc Vinyals <marc.vinyals@tifr.res.in>
2019-05-21 14:45:36 +0530

    Add test for issue 18

---
+++ test/BurstTest.java
@@ -0,0 +1,22 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+import com.gitlab.pdftk_java.pdftk;
+
+public class BurstTest extends BlackBoxTest {
+  @Rule
+  public final TemporaryFolder tmpDirectory = new TemporaryFolder();
+
+  @Test
+  public void burst_issue18() throws IOException {
+    exit.expectSystemExitWithStatus(0);
+    String pattern = tmpDirectory.getRoot().getPath() + "/page%04d.pdf";
+    pdftk.main(new String[]{"test/files/issue18.pdf", "burst", "output", pattern});
+    Files.delete(Paths.get("doc_data.txt"));
+  }
+};
