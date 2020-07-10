a58e0cb9e2fc9ce17d4a4198dc31e233ecc86be2
Marc Vinyals <vinyals@kth.se>
2020-07-11 00:27:36 +0300

    Simplify keyword parsing

--- java/com/gitlab/pdftk_java/TK_Session.java
+++ java/com/gitlab/pdftk_java/TK_Session.java
@@ -152,7 +152,7 @@ class TK_Session {
     attach_file_k, // attach files to output
     update_info_k,
     update_info_utf8_k, // if info isn't utf-8, it is encoded using xml entities
-    update_xmp_k,
+    // update_xmp_k,
     background_k, // promoted from output option to operation in pdftk 1.10
     multibackground_k, // feature added by Bernhard R. Link <brlink@debian.org>, Johann Felix Soden
     // <johfel@gmx.de>
@@ -221,42 +221,26 @@ class TK_Session {
   static keyword is_keyword(String ss) {
     ss = ss.toLowerCase();
 
+    try {
+      keyword k = keyword.valueOf(ss + "_k");
+      return k;
+    } catch (IllegalArgumentException e) {
+    }
+
     // operations
-    if (ss.equals("cat")) {
-      return keyword.cat_k;
-    } else if (ss.equals("shuffle")) {
-      return keyword.shuffle_k;
-    } else if (ss.equals("burst")) {
-      return keyword.burst_k;
-    } else if (ss.equals("filter")) {
-      return keyword.filter_k;
-    } else if (ss.equals("dump_data")
-        || ss.equals("dumpdata")
-        || ss.equals("data_dump")
-        || ss.equals("datadump")) {
+    if (ss.equals("dumpdata") || ss.equals("data_dump") || ss.equals("datadump")) {
       return keyword.dump_data_k;
-    } else if (ss.equals("dump_data_utf8")) {
-      return keyword.dump_data_utf8_k;
-    } else if (ss.equals("dump_data_fields")) {
-      return keyword.dump_data_fields_k;
-    } else if (ss.equals("dump_data_fields_utf8")) {
-      return keyword.dump_data_fields_utf8_k;
-    } else if (ss.equals("dump_data_annots")) {
-      return keyword.dump_data_annots_k;
-    } else if (ss.equals("generate_fdf")
-        || ss.equals("fdfgen")
-        || ss.equals("fdfdump")
-        || ss.equals("dump_data_fields_fdf")) {
+    } else if (ss.equals("fdfgen") || ss.equals("fdfdump") || ss.equals("dump_data_fields_fdf")) {
       return keyword.generate_fdf_k;
-    } else if (ss.equals("fill_form") || ss.equals("fillform")) {
+    } else if (ss.equals("fillform")) {
       return keyword.fill_form_k;
-    } else if (ss.equals("attach_file") || ss.equals("attach_files") || ss.equals("attachfile")) {
+    } else if (ss.equals("attach_files") || ss.equals("attachfile")) {
       return keyword.attach_file_k;
-    } else if (ss.equals("unpack_file") || ss.equals("unpack_files") || ss.equals("unpackfiles")) {
+    } else if (ss.equals("unpack_file") || ss.equals("unpackfiles")) {
       return keyword.unpack_files_k;
-    } else if (ss.equals("update_info") || ss.equals("updateinfo")) {
+    } else if (ss.equals("updateinfo")) {
       return keyword.update_info_k;
-    } else if (ss.equals("update_info_utf8") || ss.equals("updateinfoutf8")) {
+    } else if (ss.equals("updateinfoutf8")) {
       return keyword.update_info_utf8_k;
     }
     /* requires more testing and work
@@ -265,19 +249,6 @@ class TK_Session {
       return update_xmp_k;
     }
     */
-    else if (ss.equals("background")) {
-      // pdftk 1.10: making background an operation
-      // (and preserving old behavior for backwards compatibility)
-      return keyword.background_k;
-    } else if (ss.equals("multibackground")) {
-      return keyword.multibackground_k;
-    } else if (ss.equals("multistamp")) {
-      return keyword.multistamp_k;
-    } else if (ss.equals("stamp")) {
-      return keyword.stamp_k;
-    } else if (ss.equals("rotate")) {
-      return keyword.rotate_k;
-    }
 
     // cat range keywords
     else if (ss.startsWith("even")) { // note: strncmp
@@ -287,26 +258,23 @@ class TK_Session {
     }
 
     // file attachment option
-    else if (ss.equals("to_page") || ss.equals("topage")) {
+    else if (ss.equals("topage")) {
       return keyword.attach_file_to_page_k;
-    } else if (ss.equals("output")) {
-      return keyword.output_k;
     }
 
     // encryption & decryption; depends on context
-    else if (ss.equals("owner_pw") || ss.equals("ownerpw")) {
+    else if (ss.equals("ownerpw")) {
       return keyword.owner_pw_k;
-    } else if (ss.equals("user_pw") || ss.equals("userpw")) {
+    } else if (ss.equals("userpw")) {
       return keyword.user_pw_k;
-    } else if (ss.equals("input_pw") || ss.equals("inputpw")) {
+    } else if (ss.equals("inputpw")) {
       return keyword.input_pw_k;
     } else if (ss.equals("allow")) {
       return keyword.user_perms_k;
     }
 
     // expect these only in output section
-    else if (ss.equals("encrypt_40bit")
-        || ss.equals("encrypt_40bits")
+    else if (ss.equals("encrypt_40bits")
         || ss.equals("encrypt40bit")
         || ss.equals("encrypt40bits")
         || ss.equals("encrypt40_bit")
@@ -314,8 +282,7 @@ class TK_Session {
         || ss.equals("encrypt_40_bit")
         || ss.equals("encrypt_40_bits")) {
       return keyword.encrypt_40bit_k;
-    } else if (ss.equals("encrypt_128bit")
-        || ss.equals("encrypt_128bits")
+    } else if (ss.equals("encrypt_128bits")
         || ss.equals("encrypt128bit")
         || ss.equals("encrypt128bits")
         || ss.equals("encrypt128_bit")
@@ -344,28 +311,15 @@ class TK_Session {
       return keyword.perm_degraded_printing_k;
     } else if (ss.equals("allfeatures")) {
       return keyword.perm_all_k;
-    } else if (ss.equals("uncompress")) {
+    }
+
+    // pdftk keywords
+    else if (ss.equals("uncompress")) {
       return keyword.filt_uncompress_k;
     } else if (ss.equals("compress")) {
       return keyword.filt_compress_k;
-    } else if (ss.equals("flatten")) {
-      return keyword.flatten_k;
-    } else if (ss.equals("need_appearances")) {
-      return keyword.need_appearances_k;
-    } else if (ss.equals("drop_xfa")) {
-      return keyword.drop_xfa_k;
-    } else if (ss.equals("drop_xmp")) {
-      return keyword.drop_xmp_k;
-    } else if (ss.equals("keep_first_id")) {
-      return keyword.keep_first_id_k;
-    } else if (ss.equals("keep_final_id")) {
-      return keyword.keep_final_id_k;
-    } else if (ss.equals("verbose")) {
-      return keyword.verbose_k;
-    } else if (ss.equals("dont_ask") || ss.equals("dontask")) {
+    } else if (ss.equals("dontask")) {
       return keyword.dont_ask_k;
-    } else if (ss.equals("do_ask")) {
-      return keyword.do_ask_k;
     }
 
     // more cat range keywords
