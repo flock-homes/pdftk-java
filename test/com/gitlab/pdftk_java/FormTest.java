afde47502d47385b9b33c2c66ba424b29c8e22e1
Marc Vinyals <vinyals@kth.se>
2021-04-23 13:02:15 +0300

    Add test for issue 88

--- test/com/gitlab/pdftk_java/FormTest.java
+++ test/com/gitlab/pdftk_java/FormTest.java
@@ -21,6 +21,13 @@ public class FormTest extends BlackBox {
   }
 
   @Test
+  public void generate_fdf_issue88() throws IOException {
+    pdftk("test/files/issue88.pdf", "generate_fdf", "output", "-");
+    byte[] expectedData = slurpBytes("test/files/issue88.fdf");
+    assertArrayEquals(expectedData, systemOut.getLogAsBytes());
+  }
+
+  @Test
   public void fill_from_fdf() throws IOException {
     pdftk("test/files/form.pdf", "fill_form", "test/files/form-filled.fdf", "output", "-");
   }
